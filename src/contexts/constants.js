8882b8a79aad4ef3baf93f7e26961f56d8b1b036
KhanhGCH200087 <khanhpmgch200087@fpt.edu.vn>
2024-05-03 04:41:31 +0700

    Update constants.js

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,5 +1,5 @@
-export const apiUrl = 'https://deploytest-v6ey.onrender.com'
+export const apiUrl = 'https://api.render.com/deploy/srv-copt345jm4es73ab7dq0?key=Qs6yfViz1oY'
 
-export const LOCAL_URL = 'https://deploytest-v6ey.onrender.com'
+export const LOCAL_URL = 'https://api.render.com/deploy/srv-copt345jm4es73ab7dq0?key=Qs6yfViz1oY'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'test'
