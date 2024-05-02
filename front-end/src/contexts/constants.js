36251572da38cba575ca44fb1392bdbd10aab12b
KhanhGCH200087 <khanhpmgch200087@fpt.edu.vn>
2024-05-03 03:54:27 +0700

    Update constants.js

--- front-end/src/contexts/constants.js
+++ front-end/src/contexts/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl = 'https://deploytest-v6ey.onrender.com'
 
-export const LOCAL_URL = 'http://localhost:3000'
+export const LOCAL_URL = 'https://deploytest-v6ey.onrender.com'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'test'
